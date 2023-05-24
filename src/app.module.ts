7979a573dbf6c5aab5b3351a1e75992ea29bc6be
Violacordis <ikemviolacordis@gmail.com>
2023-05-24 15:33:33 +0100

    chores(created user, url and user folders)

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,15 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaService } from './prisma/prisma.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { UserModule } from './user/user.module';
+import { AuthController } from './auth/auth.controller';
+import { AuthService } from './auth/auth.service';
+import { UrlService } from './url/url.service';
+import { UrlController } from './url/url.controller';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule],
-  controllers: [],
-  providers: [PrismaService],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule, UserModule],
+  controllers: [AuthController, UrlController],
+  providers: [PrismaService, AuthService, UrlService],
 })
 export class AppModule {}
