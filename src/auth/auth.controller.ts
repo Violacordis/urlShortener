8ff8cce2407d12a05774a2f7bbc01b8680ef9f05
Violacordis <ikemviolacordis@gmail.com>
2023-05-26 15:05:50 +0100

    chore(Auth controller): added a message when a user logs in successfully

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,6 +2,7 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { loginDto, signUpDto } from './dto';
 import { ApiTags } from '@nestjs/swagger';
+import { ApiResponseMetadata } from 'src/utils/decorators';
 
 @ApiTags('Auth')
 @Controller('auth')
@@ -13,6 +14,7 @@ export class AuthController {
     return this.authService.signUp(dto);
   }
 
+  @ApiResponseMetadata({ message: 'You have logged in!' })
   @Post('login')
   async login(@Body() dto: loginDto) {
     return this.authService.login(dto);
